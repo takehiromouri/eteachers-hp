0f6f980c9367fe827c99a27e10a5830c6cdf2d93
Take Mouri <takehiromouri@gmail.com>
2016-07-27 16:25:03 +0545

    init

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function ready() {
+
+  var url = window.location.pathname;
+  // Will only work if string in href matches with location
+  $('ul.nav-items li a[href="'+ url +'"]').parent().addClass('active');
+
+  $('div.sidebar-nav-items ul a[href="'+ url +'"] li' ).addClass('active');
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
