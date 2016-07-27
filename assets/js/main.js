f55290b54453523ecc8ceb107acc52d87927921a
Take Mouri <takehiromouri@gmail.com>
2016-07-27 17:05:03 +0545

    asd

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ function ready() {
 
   var url = window.location.pathname;
   // Will only work if string in href matches with location
-  $('ul.nav-items li a[href="'+ url +'"]').parent().addClass('active');
+  $('ul.nav-items li a[href="../'+ url +'"]').parent().addClass('active');
 
-  $('div.sidebar-nav-items ul a[href="'+ url +'"] li' ).addClass('active');
+  $('div.sidebar-nav-items ul a[href="../'+ url +'"] li' ).addClass('active');
 }
 
 $(document).ready(ready);
